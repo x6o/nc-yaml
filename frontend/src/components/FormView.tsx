367b574201c8b6010c55a6349c67d60426d55cf3
Andrei Talpa <talpa.andrei@gmail.com>
2025-11-13 08:55:14 +0100

    FE: Form view basic form

--- frontend/src/components/FormView.tsx
+++ frontend/src/components/FormView.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import {
   Paper,
   Typography,
+  TextField,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  Switch,
   Box,
+  Divider,
 } from '@mui/material';
 
 interface FormViewProps {
@@ -17,7 +25,57 @@ const FormView: React.FC<FormViewProps> = ({ }) => {
       </Typography>
 
       <Box>
-        Form goes here
+        <Typography>
+          Server Configuration
+        </Typography>
+
+        <TextField
+          fullWidth
+          label="Host"
+          value="Host val"
+          helperText="Hostname or IP address"
+        />
+
+        <TextField
+          fullWidth
+          label="Port"
+          type="number"
+          value="port val"
+        />
+
+        <FormControlLabel
+          control={
+            <Switch
+              checked={true}
+            />
+          }
+          label="Enable SSL"
+        />
+
+        <Divider />
+
+        <Typography >
+          Logging Configuration
+        </Typography>
+
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Log Level</FormLabel>
+          <RadioGroup
+            value="debug"
+          >
+
+            <FormControlLabel value="debug" control={<Radio />} label="Debug" />
+            <FormControlLabel value="info" control={<Radio />} label="Info" />
+            <FormControlLabel value="warn" control={<Radio />} label="Warn" />
+            <FormControlLabel value="error" control={<Radio />} label="Error" />
+          </RadioGroup>
+        </FormControl>
+
+        <TextField
+          fullWidth
+          label="Log File"
+          value="Log file"
+        />
       </Box>
     </Paper>
   );
