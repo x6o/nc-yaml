c0ce812b852e073bfb1b447e3ddc9dd6d8debf2d
Andrei Talpa <talpa.andrei@gmail.com>
2025-11-13 09:27:14 +0100

    BE: expand on get and save endpoints, add config schema

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,63 @@
+import os
+import yaml
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from config_schema import DEFAULT_CONFIG
 
 app = Flask(__name__)
 CORS(app)
 
+CONFIG_FILE = 'config.yaml'
+
+def ensure_config_file_exists():
+    """Create default config.yamlif it doesn't exist"""
+    if not os.path.exists(CONFIG_FILE):
+        write_config(DEFAULT_CONFIG)
+
+def read_config():
+    """Read configuration from YAML file."""
+    ensure_config_file_exists()
+    with open(CONFIG_FILE, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+def write_config(config_data):
+    """Write configuration to YAML file."""
+    with open(CONFIG_FILE, 'w') as f:
+        yaml.dump(config_data, f, default_flow_style=False, sort_keys=False)
+
 @app.route('/api/config', methods=['GET'])
 def get_config():
-    """
-    Get config from fs
-    """
-    return jsonify({
-        "success": True,
-        "data": {}
-    }), 200
+    """Get config from filesystem"""
+    try:
+        config = read_config()
+        return jsonify({
+            "success": True,
+            "data": config
+            }), 200
+    except Exception as e:
+        return jsonify({"success": False, "error": e}), 500
+
 
 @app.route('/api/config', methods=['PUT'])
 def update_config():
-    """
-    PUT config from FS
-    """
-    return jsonify({
-        "success": True,
-        "message": "Configuration updated successfully"
-    }), 200
+    """PUT config to filesystem"""
+    try:
+        new_config = request.get_json()
+
+        if not new_config:
+            return jsonify({"success": False, "error": "No data provided"}), 400
+
+        # TODO: Validate config
+
+        write_config(validated_config)
+
+        return jsonify({
+            "success": True,
+            "message": "Configuration updated successfully"
+        }), 200
+    except Exception as e:
+        return jsonify({"success": False, "error": f"Server error: {e}"}), 500
 
 
 if __name__ == '__main__':
---
+++ backend/config_schema.py
@@ -0,0 +1,12 @@
+# Default configuration
+DEFAULT_CONFIG = {
+    "server": {
+        "host": "127.0.0.1",
+        "port": 3000,
+        "use_ssl": True
+    },
+    "logging": {
+        "level": "debug",
+        "file": "./debug.log"
+    }
+}
